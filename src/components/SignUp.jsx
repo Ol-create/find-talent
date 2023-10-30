d72a62a63bb98f5a9f74a7c9d2735327e52a2d4d
Ol-create <oluola96@gmail.com>
2023-10-30 23:23:03 +0100

    add log in option

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -11,8 +11,11 @@ export default function SignUp() {
         </a>
       </div>
       {/* <div className="flex justify-center"> */}
-      <div className="w-full h-99 bg-gray-200 border border-gray-700 ">
-        <h1 className="mx-auto w-1/2">Join as a client or freelancer</h1>
+      <div className="w-full h-99 bg-gray-200 border border-gray-700">
+        <h1 className="text-center w-full text-2xl md:text-4xl pt-4 md:pt-6 h-24 ">
+          Join as a client or <br className="sm:hidden" />
+          freelancer
+        </h1>
         <div className="border border-gray-400 rounded-lg w-[90%] mx-auto h-32 mb-6">
           <div className="flex justify-between ">
             <img src={logo} alt="logo" className="w-12 h-10 m-5 block" />
@@ -31,10 +34,20 @@ export default function SignUp() {
           </div>
           <p className="pl-2 text-lg">I’m a freelancer, looking for work</p>
         </div>
-        <div className="w-[90%] mx-auto mt-9">
-          <button type="submit" disabled={true} className="rounded-lg bg-gray-300 cursor-not-allowed text-gray-500 w-full h-9">
+        <div className="w-[90%] mx-auto my-9">
+          <button
+            type="submit"
+            disabled={true}
+            className="rounded-full bg-gray-300 cursor-not-allowed text-gray-500 w-full h-9"
+          >
             Create Account
           </button>
+        </div>
+        <div className="flex justify-center my-4">
+          <p>Already have an account?</p>
+          <a href="" className="text-green-600 ml-1">
+            Log In
+          </a>
         </div>
       </div>
       {/* </div> */}
