76710d40a63e7cc6da074d56fe484a7730804f02
Ol-create <oluola96@gmail.com>
2023-11-14 18:10:59 +0100

    add talent category section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ import icon from "../images/google-icon.png"
 
 export default function Home() {
   return (
-    <div className="bg-blue-600 container mx-auto px-4">
+    <div className="container mx-auto px-4">
       <h1>How work should work</h1>
       <h3>
         Forget the old rules. You can have the best people. Right now. Right
@@ -37,6 +37,138 @@ export default function Home() {
               a consultation.
             </p>
           </div>
+        </div>
+        <div className="flex space-x-3">
+          <img src={icon} alt="No cost to join icon" className="w-4 h-4" />
+          <div>
+            <h3>Post a job and hire top talent</h3>
+            <p className="text-xs">
+              Finding talent doesn’t have to be a chore. Post a job or we can
+              search for you!
+            </p>
+          </div>
+        </div>
+        <div className="flex space-x-3">
+          <img src={icon} alt="No cost to join icon" className="w-4 h-4" />
+          <div>
+            <h3>Work with the best—without breaking the bank</h3>
+            <p className="text-xs">
+              Upwork makes it affordable to up your work and take advantage of
+              low transaction rates.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Start of the button */}
+      <a
+        href=""
+        className="block border border-green-300 text-center rounded-full mt-3 h-9 py-1 hover:bg-gray-200"
+      >
+        Learn hoe to hire
+      </a>
+      <h2>Browse talent by category</h2>
+      <p>
+        Looking for work?{" "}
+        <a href="" className="text-green-600 underline">
+          Browse jobs
+        </a>
+      </p>
+      <div className='grid md:grid-cols-2 xl:grid-cols-4 gap-x-5 gap-y-2'>
+        <div className="bg-gray-200 rounded-md p-2">
+          <h3 className="text-xl">Development & IT</h3>
+          <p className="flex space-x-3 items-center">
+            <p className="bg-red-300 flex items-center">
+              <span className="material-icons text-green-600 bg-yellow-400">
+                star
+              </span>
+              <span className="bg-green-300">4.85/5</span>
+            </p>
+            <p className="">1853 skills</p>
+          </p>
+        </div>
+        <div className="bg-gray-200 rounded-md p-2">
+          <h3 className="text-xl">AI Services </h3>
+          <p className="flex space-x-3 items-center">
+            <p className="bg-red-300 flex items-center">
+              <span className="material-icons text-green-600 bg-yellow-400">
+                star
+              </span>
+              <span className="bg-green-300">4.8/5 </span>
+            </p>
+            <p className="">294 skills</p>
+          </p>
+        </div>
+        <div className="bg-gray-200 rounded-md p-2">
+          <h3 className="text-xl">Design & Creative </h3>
+          <p className="flex space-x-3 items-center">
+            <p className="bg-red-300 flex items-center">
+              <span className="material-icons text-green-600 bg-yellow-400">
+                star
+              </span>
+              <span className="bg-green-300">4.91/5 </span>
+            </p>
+            <p className="">968 skills</p>
+          </p>
+        </div>
+        <div className="bg-gray-200 rounded-md p-2">
+          <h3 className="text-xl">Sales & Marketing </h3>
+          <p className="flex space-x-3 items-center">
+            <p className="bg-red-300 flex items-center">
+              <span className="material-icons text-green-600 bg-yellow-400">
+                star
+              </span>
+              <span className="bg-green-300">4.77/5 </span>
+            </p>
+            <p className="">392 skills</p>
+          </p>
+        </div>
+        <div className="bg-gray-200 rounded-md p-2">
+          <h3 className="text-xl">Writing & Translation </h3>
+          <p className="flex space-x-3 items-center">
+            <p className="bg-red-300 flex items-center">
+              <span className="material-icons text-green-600 bg-yellow-400">
+                star
+              </span>
+              <span className="bg-green-300">4.92/5 </span>
+            </p>
+            <p className="">505 skills</p>
+          </p>
+        </div>
+        <div className="bg-gray-200 rounded-md p-2">
+          <h3 className="text-xl">Admin & Customer Support </h3>
+          <p className="flex space-x-3 items-center">
+            <p className="bg-red-300 flex items-center">
+              <span className="material-icons text-green-600 bg-yellow-400">
+                star
+              </span>
+              <span className="bg-green-300">4.77/5 </span>
+            </p>
+            <p className="">508 skills</p>
+          </p>
+        </div>
+        <div className="bg-gray-200 rounded-md p-2">
+          <h3 className="text-xl">Finance & Accounting </h3>
+          <p className="flex space-x-3 items-center">
+            <p className="bg-red-300 flex items-center">
+              <span className="material-icons text-green-600 bg-yellow-400">
+                star
+              </span>
+              <span className="bg-green-300">4.79/5 </span>
+            </p>
+            <p className="">214 skills</p>
+          </p>
+        </div>
+        <div className="bg-gray-200 mt-2 rounded-md p-2">
+          <h3 className="text-xl">Engineering & Architecture</h3>
+          <p className="flex space-x-3 items-center">
+            <p className="bg-red-300 flex items-center">
+              <span className="material-icons text-green-600 bg-yellow-400">
+                star
+              </span>
+              <span className="bg-green-300">4.85/5 </span>
+            </p>
+            <p className="">650 skills</p>
+          </p>
         </div>
       </div>
     </div>
