d18b827f51fc62baea8de9b660c64a9f42fda3bd
Ol-create <oluola96@gmail.com>
2025-01-02 18:36:30 +0100

    add style to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 import NavFindTalent from "../common/NavFindTalent";
@@ -6,12 +6,15 @@ import NavFindWork from "../common/NavFindWork";
 import WhyUpwork from "../common/WhyUpwork";
 import WhatsNew from "../common/WhatsNew";
 import SubNavBar from "../common/SubNavBar";
+import { grey } from "@mui/material/colors";
 
 export default function NavBar() {
   const [findTalent, setFindTalent] = useState(false);
   const [isFindWork, setFindWork] = useState(false);
   const [whyUpwork, setWhyUpwork] = useState(false);
   const [isWhatsNew, setWhatsNew] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  const divRef = useRef(null);
 
   const handleOnMouseOver = () => {
     setFindTalent(true);
@@ -42,6 +45,29 @@ export default function NavBar() {
   const handleOnMouseLeaveWhatsNew = () => {
     setWhatsNew(false);
   };
+
+  // Function to handle the click inside the div
+  const handleDivClick = (event) => {
+    // Prevent the click event from propagating to the document
+    event.stopPropagation();
+    setIsClicked(true);
+  };
+
+  // Function to handle clicks outside the div
+  const handleClickOutside = (event) => {
+    if (divRef.current && !divRef.current.contains(event.target)) {
+      setIsClicked(false);
+    }
+  };
+
+  // Set up event listener for clicks outside the div
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
@@ -116,16 +142,21 @@ export default function NavBar() {
           </ul>
         </div>
         <div className="flex space-x-5 items-center pb-3">
-          <div className="relative flex border-2 border-gray-200 hover:bg-gray-200 rounded-full px-8 h-10 w-80 ">
+          <div className="relative flex border-0 border-gray-200 bg-gray-200 rounded-full px-8 h-10 w-80 ">
           {/*  */}
+              <div className="hover:bg-gray-100">
               <img src="/images/search_icon.png" 
                    alt="search"
                    className="w-6 h-6 ml-2 absolute left-0 top-[7px] z-10" />
-              <input className="absolute left-0 outline-none rounded-full border-l-0  pl-10 pr-3 h-9 w-4/6 hover:bg-gray-100 focus:bg-white" 
+              <input className="absolute left-0 outline-none h-10 rounded-full border-2  pl-10 pr-3 w-4/6 hover:bg-gray-100 bg-gray-200 focus:bg-white focus:border-gray-400" 
                      type="text" 
                      placeholder="Search" />
-            <div className="hover:bg-gray-100 absolute right-0 h-9 rounded-full w-2/6 text-center items-center flex justify-center">
-            <p>Talent</p>
+              </div>
+            <div 
+             className={`hover:bg-gray-100 absolute right-0 h-10 bg-gray-200 rounded-full w-2/6 text-center items-center flex justify-center border-2  ${isClicked ? "border-gray-400 bg-white hover:bg-white" : ""}`}
+             ref={divRef}
+            onClick={handleDivClick}>
+            <p> Talent </p>
              <img
                 src="/images/keyboard-arrow-down.png"
                 alt=""
@@ -166,3 +197,4 @@ export default function NavBar() {
     </>
   );
 }
+
